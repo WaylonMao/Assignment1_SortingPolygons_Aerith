ce95fafc34c0ed1e3fb7bc36cdf12909efee585c
WaylonM <weilong.mao@edu.sait.ca>
2022-10-01 21:48:25 -0600

    Update to Hoa's version

--- src/com/aerith/sortingpolygons/sorts/QuickSort.java
+++ src/com/aerith/sortingpolygons/sorts/QuickSort.java
@@ -9,100 +9,129 @@ import java.util.Comparator;
  * When using this class you do not need to create instances, because they are all static methods.
  * And they already include both ascending and descending methods.
  *
- * @author Weilong Mao
+ * @author Hoa Le, Weilong Mao
  */
 public class QuickSort {
-    public static <T extends Comparable<? super T>> void quickSort(T[] arrays) {
-        doQuick(arrays, 0, arrays.length - 1);
-    }
-
-    public static <T extends Comparable<? super T>> void quickSort(T[] arrays, Comparator<? super T> comparator) {
-        doQuick(arrays, 0, arrays.length - 1, comparator);
+    public static <T extends Comparable<? super T>> void quickSort(T[] shape, Comparator<? super T> comparator) {
+        doQuick(shape, comparator, 0, shape.length - 1);
     }
 
 
-    private static <T extends Comparable<? super T>> void doQuick(T[] arrays, int left, int right) {
-        if (left < right) {
-            T pivot = arrays[left];
-            int i = left;
-            int j = right;
-            while (i < j) {
-                while (i < j && arrays[i].compareTo(pivot) <= 0) i++;
-                while (i < j && arrays[j].compareTo(pivot) >= 0) j--;
-                swap(arrays, i, j);
-            }
-            arrays[left] = arrays[i];
-            arrays[i] = pivot;
-            doQuick(arrays, left, j - 1);
-            doQuick(arrays, j + 1, right);
-        }
+    public static <T extends Comparable<? super T>> void quickSort(T[] shape) {
+        doQuick(shape, 0, shape.length - 1);
     }
 
-    private static <T extends Comparable<? super T>> void doQuick(T[] arrays, int left, int right, Comparator<? super T> comparator) {
-        if (left < right) {
-            T pivot = arrays[left];
-            int i = left;
-            int j = right;
-            while (i < j) {
-                while (i < j && comparator.compare(arrays[i], pivot) <= 0) i++;
-                while (i < j && comparator.compare(arrays[j], pivot) >= 0) j--;
-                swap(arrays, i, j);
-            }
-            arrays[left] = arrays[i];
-            arrays[i] = pivot;
-            doQuick(arrays, left, j - 1, comparator);
-            doQuick(arrays, j + 1, right, comparator);
-        }
-    }
-
-    public static <T extends Comparable<? super T>> void quickSortDesc(T[] arrays) {
-        doQuickDesc(arrays, 0, arrays.length - 1);
-    }
-
-    public static <T extends Comparable<? super T>> void quickSortDesc(T[] arrays, Comparator<? super T> comparator) {
-        doQuickDesc(arrays, 0, arrays.length - 1, comparator);
-    }
-
-    private static <T extends Comparable<? super T>> void doQuickDesc(T[] arrays, int left, int right) {
-        if (left < right) {
-            T pivot = arrays[left];
-            int i = left;
-            int j = right;
-            while (i < j) {
-                while (i < j && arrays[i].compareTo(pivot) >= 0) i++;
-                while (i < j && arrays[j].compareTo(pivot) <= 0) j--;
-                swap(arrays, i, j);
-            }
-            arrays[left] = arrays[i];
-            arrays[i] = pivot;
-            doQuickDesc(arrays, left, j - 1);
-            doQuickDesc(arrays, j + 1, right);
-        }
-    }
-
-    private static <T extends Comparable<? super T>> void doQuickDesc(T[] arrays, int left, int right, Comparator<? super T> comparator) {
-        if (left < right) {
-            T pivot = arrays[left];
-            int i = left;
-            int j = right;
-            while (i < j) {
-                while (i < j && comparator.compare(arrays[i], pivot) >= 0) i++;
-                while (i < j && comparator.compare(arrays[j], pivot) <= 0) j--;
-                swap(arrays, i, j);
-            }
-            arrays[left] = arrays[i];
-            arrays[i] = pivot;
-            doQuickDesc(arrays, left, j - 1, comparator);
-            doQuickDesc(arrays, j + 1, right, comparator);
+    public static <T extends Comparable<? super T>> void doQuick(T[] shape, Comparator<? super T> comparator, int low, int high) {
+        if (low < high) {
+            int pi = partition(shape, comparator, low, high);
+            doQuick(shape, comparator, low, pi - 1);
+            doQuick(shape, comparator, pi + 1, high);
         }
     }
 
 
-    public static <T> void swap(T[] o, int a, int b) {
-        T temp = o[a];
-        o[a] = o[b];
-        o[b] = temp;
+    public static <T extends Comparable<? super T>> void doQuick(T[] shape, int low, int high) {
+        if (low < high) {
+            int pi = partition(shape, low, high);
+            doQuick(shape, low, pi - 1);
+            doQuick(shape, pi + 1, high);
+        }
     }
 
+    private static <T extends Comparable<? super T>> int partition(T[] shape, Comparator<? super T> comparator, int low, int high) {
+        // select pivot
+        T pivot = shape[high];
+        int i = low;
+        for (int j = low; j <= high - 1; j++) {
+            // if current element is smaller than pivot, swap between those elements
+            if (comparator.compare(shape[j], pivot) < 0) {
+                T temp = shape[i];
+                shape[i] = shape[j];
+                shape[j] = temp;
+                i++;
+            }
+        }
+        shape[high] = shape[i];
+        shape[i] = pivot;
+        return i;
+
+    }
+
+    private static <T extends Comparable<? super T>> int partition(T[] shape, int low, int high) {
+        // select pivot
+        T pivot = shape[high];
+        int i = low;
+        for (int j = low; j <= high - 1; j++) {
+            // if current element is smaller than pivot, swap between those elements
+            if (shape[j].compareTo(pivot) < 0) {
+                T temp = shape[i];
+                shape[i] = shape[j];
+                shape[j] = temp;
+                i++;
+            }
+        }
+        shape[high] = shape[i];
+        shape[i] = pivot;
+        return i;
+    }
+
+    public static <T extends Comparable<? super T>> void quickSortDesc(T[] shape, Comparator<? super T> comparator) {
+        doQuickDesc(shape, comparator, 0, shape.length - 1);
+    }
+
+    public static <T extends Comparable<? super T>> void quickSortDesc(T[] shape) {
+        doQuickDesc(shape, 0, shape.length - 1);
+    }
+
+    public static <T extends Comparable<? super T>> void doQuickDesc(T[] shape, Comparator<? super T> comparator, int low, int high) {
+        if (low < high) {
+            int pi = partitionDesc(shape, comparator, low, high);
+            doQuickDesc(shape, comparator, low, pi - 1);
+            doQuickDesc(shape, comparator, pi + 1, high);
+        }
+    }
+
+    public static <T extends Comparable<? super T>> void doQuickDesc(T[] shape, int low, int high) {
+        if (low < high) {
+            int pi = partitionDesc(shape, low, high);
+            doQuickDesc(shape, low, pi - 1);
+            doQuickDesc(shape, pi + 1, high);
+        }
+    }
+
+    private static <T extends Comparable<? super T>> int partitionDesc(T[] shape, Comparator<? super T> comparator, int low, int high) {
+        // select pivot
+        T pivot = shape[high];
+        int i = low;
+        for (int j = low; j <= high - 1; j++) {
+            // if current element is smaller than pivot, swap between those elements
+            if (comparator.compare(shape[j], pivot) > 0) {
+                T temp = shape[i];
+                shape[i] = shape[j];
+                shape[j] = temp;
+                i++;
+            }
+        }
+        shape[high] = shape[i];
+        shape[i] = pivot;
+        return i;
+    }
+
+    private static <T extends Comparable<? super T>> int partitionDesc(T[] shape, int low, int high) {
+        // select pivot
+        T pivot = shape[high];
+        int i = low;
+        for (int j = low; j <= high - 1; j++) {
+            // if current element is smaller than pivot, swap between those elements
+            if (shape[j].compareTo(pivot) > 0) {
+                T temp = shape[i];
+                shape[i] = shape[j];
+                shape[j] = temp;
+                i++;
+            }
+        }
+        shape[high] = shape[i];
+        shape[i] = pivot;
+        return i;
+    }
 }
-
